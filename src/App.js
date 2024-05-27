801c6aeb3398a6631fc838283ddf466cf61704c4
3th-enjay <enjayjerey@gmail.com>
2024-05-27 07:13:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Profile from './components/profile';
 
 function App() {
+
+  const users = [
+    { Name: 'Nancy', Boi: 'choir', DateOfBirth: '29/05/1997' },
+    { Name: ' Nelson ', Boi: 'artist', DateOfBirth: '13/05/2003' },
+    { Name: 'Nonso', Boi: 'creator', DateOfBirth: '04/02/1998' },
+    { Name: 'Nelly', Boi: 'comedy', DateOfBirth: '09/06/2000' },
+    { name: 'Nickel', }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "p-" >
+      <Profile users={users} />
     </div>
   );
 }
